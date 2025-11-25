d64ef8c8e642b203b60da2d3e2304bed22fb6790
vinsa61 <vinsarioshen610@gmail.com>
2025-11-25 19:22:38 +0700

    initial commit

--- node_a/run.bash
+++ node_a/run.bash
@@ -1,9 +1,11 @@
 #!/bin/bash
+PEERS="A@192.168.1.1:5000 B@192.168.1.2:5000 C@192.168.1.3:5000 D@192.168.1.4:5000"
 
 python3 peer_node.py \
-	  --name A --listen 0.0.0.0 5000 \
-	  --peers A@localhost:5000 B@localhost:5001 D@localhost:5002 \
-	  --logger localhost 9999 \
-	  --offset-ms 600 \
-	  --initiate-broadcast --msg "Hello from A"
-
+  --name A \
+  --listen 0.0.0.0 5000 \
+  --peers $PEERS \
+  --logger 192.168.1.100 9999 \
+  --offset-ms 600 \
+  --initiate-broadcast \
+  --msg "Hello from A"
--- node_b/run.bash
+++ node_b/run.bash
@@ -1,8 +1,9 @@
 #!/bin/bash
+PEERS="A@192.168.1.1:5000 B@192.168.1.2:5000 C@192.168.1.3:5000 D@192.168.1.4:5000"
 
 python3 peer_node.py \
-	  --name B --listen 0.0.0.0 5001 \
-	  --peers A@localhost:5000 B@localhost:5001 D@localhost:5002 \
-	  --logger localhost 9999 \
-	  --offset-ms -600 
-
+  --name B \
+  --listen 0.0.0.0 5000 \
+  --peers $PEERS \
+  --logger 192.168.1.100 9999 \
+  --offset-ms -200
